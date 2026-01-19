0f862c8ea8e3543706be702c4f72fafb156e60b8
takogagnidze <takogagnidze18@gmail.com>
2026-01-20 01:12:58 +0400

    Added bet Dtos and roulette service

---
+++ VirtualRouletteApi/Domain/Bet.cs
@@ -0,0 +1,20 @@
+﻿namespace VirtualRouletteApi.Domain;
+
+public class Bet
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid UserId { get; set; }
+    
+    public required string BetJson { get; set; }
+
+    public long BetAmount { get; set; }
+
+    public int WinningNumber { get; set; }
+
+    public long WinAmount { get; set; }
+
+    public string IpAddress { get; set; } = string.Empty;
+
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
